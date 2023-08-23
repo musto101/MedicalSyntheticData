d1973a5bf124990c7e70f0a738b32ed211f87135
Henry Musto <musto101@googlemail.com>
2023-08-23 16:53:09 +0100

    First VAE model with decent loss complete.

--- VAE.py
+++ VAE.py
@@ -2,56 +2,100 @@ from torch import nn
 import torch
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 # create a variational autoencoder for synthetic data generation
-class VAE(nn.Module):
-    def __init__(self, latent_dim, input_dim, hidden_dim, dropout):
-        super(VAE, self).__init__()
+
+class encoder(nn.Module):
+    def __init__(self, latent_dim, input_dim, hidden_dim, dropout, activation=nn.Tanh):
+        super(encoder, self).__init__()
 
         self.latent_dim = latent_dim
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.dropout = nn.Dropout(dropout)
+        self.activation = activation
+        output_dim = 2 * self.latent_dim
+
 
         # encoder
         self.encoder = nn.Sequential(
             self.dropout,
             nn.Linear(self.input_dim, self.hidden_dim),
-            nn.ReLU(),
+            activation(),
             self.dropout,
             nn.Linear(self.hidden_dim, self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim, self.latent_dim * 2),
-        )
-
-        for layer in self.encoder:
-            if isinstance(layer, nn.Linear):
-                torch.nn.init.xavier_uniform_(layer.weight)
-                torch.nn.init.zeros_(layer.bias)
-
-        # decoder
-        self.decoder = nn.Sequential(
-            nn.Linear(self.latent_dim, self.hidden_dim),
-            nn.ReLU(),
+            activation(),
             nn.Linear(self.hidden_dim, self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim, self.input_dim),
+            activation(),
+            nn.Linear(self.hidden_dim, self.latent_dim * 2)
         )
 
-        for layer in self.decoder:
-            if isinstance(layer, nn.Linear):
-                torch.nn.init.xavier_uniform_(layer.weight)
-                torch.nn.init.zeros_(layer.bias)
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mu + eps * std
+        # for layer in self.encoder:
+        #     if isinstance(layer, nn.Linear):
+        #         torch.nn.init.xavier_uniform_(layer.weight)
+        #         torch.nn.init.zeros_(layer.bias)
 
     def forward(self, x):
         # encode
         mu_logvar = self.encoder(x)
         mu = mu_logvar[:, : self.latent_dim]
         logvar = mu_logvar[:, self.latent_dim :]
+
+        return mu, logvar
+
+class decoder(nn.Module):
+    def __init__(self, latent_dim, input_dim, hidden_dim, dropout, activation=nn.Tanh):
+        super(decoder, self).__init__()
+
+        self.latent_dim = latent_dim
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.dropout = nn.Dropout(dropout)
+        self.activation = activation
+        self.output_dim = self.input_dim
+
+        # decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(self.latent_dim, self.hidden_dim),
+            activation(),
+            nn.Linear(self.hidden_dim, self.hidden_dim),
+            activation(),
+            nn.Linear(self.hidden_dim, self.output_dim)
+        )
+
+        # for layer in self.decoder:
+        #     if isinstance(layer, nn.Linear):
+        #         torch.nn.init.xavier_uniform_(layer.weight)
+        #         torch.nn.init.zeros_(layer.bias)
+
+    def forward(self, z):
+        # decode
+        x_hat = self.decoder(z)
+
+        return x_hat
+
+class VAE(nn.Module):
+    def __init__(self, latent_dim, input_dim, hidden_dim, dropout, activation=nn.Tanh):
+        super(VAE, self).__init__()
+
+        self.latent_dim = latent_dim
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.dropout = nn.Dropout(dropout)
+        self.activation = activation
+
+        self.encoder = encoder(latent_dim, input_dim, hidden_dim, dropout, activation)
+        self.decoder = decoder(latent_dim, input_dim, hidden_dim, dropout, activation)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+
+        return mu + eps * std
+
+    def forward(self, x):
+        # encode
+        mu, logvar = self.encoder(x)
 
         # reparameterize
         z = self.reparameterize(mu, logvar)
@@ -60,6 +104,65 @@ class VAE(nn.Module):
         x_hat = self.decoder(z)
 
         return x_hat, mu, logvar
+
+
+# class VAE(nn.Module):
+#     def __init__(self, latent_dim, input_dim, hidden_dim, dropout):
+#         super(VAE, self).__init__()
+#
+#         self.latent_dim = latent_dim
+#         self.input_dim = input_dim
+#         self.hidden_dim = hidden_dim
+#         self.dropout = nn.Dropout(dropout)
+#
+#         # encoder
+#         self.encoder = nn.Sequential(
+#             self.dropout,
+#             nn.Linear(self.input_dim, self.hidden_dim),
+#             nn.ReLU(),
+#             self.dropout,
+#             nn.Linear(self.hidden_dim, self.hidden_dim),
+#             nn.ReLU(),
+#             nn.Linear(self.hidden_dim, self.latent_dim * 2),
+#         )
+#
+#         for layer in self.encoder:
+#             if isinstance(layer, nn.Linear):
+#                 torch.nn.init.xavier_uniform_(layer.weight)
+#                 torch.nn.init.zeros_(layer.bias)
+#
+#         # decoder
+#         self.decoder = nn.Sequential(
+#             nn.Linear(self.latent_dim, self.hidden_dim),
+#             nn.ReLU(),
+#             nn.Linear(self.hidden_dim, self.hidden_dim),
+#             nn.ReLU(),
+#             nn.Linear(self.hidden_dim, self.input_dim),
+#         )
+#
+#         for layer in self.decoder:
+#             if isinstance(layer, nn.Linear):
+#                 torch.nn.init.xavier_uniform_(layer.weight)
+#                 torch.nn.init.zeros_(layer.bias)
+#
+#     def reparameterize(self, mu, logvar):
+#         std = torch.exp(0.5 * logvar)
+#         eps = torch.randn_like(std)
+#         return mu + eps * std
+#
+#     def forward(self, x):
+#         # encode
+#         mu_logvar = self.encoder(x)
+#         mu = mu_logvar[:, : self.latent_dim]
+#         logvar = mu_logvar[:, self.latent_dim :]
+#
+#         # reparameterize
+#         z = self.reparameterize(mu, logvar)
+#
+#         # decode
+#         x_hat = self.decoder(z)
+#
+#         return x_hat, mu, logvar
 
 # load data
 mci = pd.read_csv('data/mci_preprocessed_wo_csf.csv')
@@ -70,10 +173,18 @@ mci = mci.drop(['Unnamed: 0'], axis=1)
 # convert to float32
 mci = mci.astype('float32')
 
-mci.dtypes
+mci.head()
 
 # drop nan values
 mci = mci.dropna()
+
+# scale data
+scaler = StandardScaler()
+mci = scaler.fit_transform(mci)
+
+
+# # split data into train and validation
+# train, val = train_test_split(mci, test_size=0.2, random_state=42)
 
 # define model
 model = VAE(latent_dim=2, input_dim=91, hidden_dim=2, dropout=0.1)
@@ -87,7 +198,7 @@ def kl_anneal_function(anneal_function, step, k, x0):
 
 
 # define optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=kl_anneal_function('logistic', 0.5, 0.0025, 2500))
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5, weight_decay=1e-5)
 
 # define loss function
 # def loss_function(x_hat, x, mu, logvar):
@@ -122,18 +233,19 @@ optimizer = torch.optim.Adam(model.parameters(), lr=kl_anneal_function('logistic
 # Loss function with separate component computations
 def loss_function(x_hat, x, mu, logvar):
     # Reconstruction loss
-    recon_loss = nn.MSELoss(reduction='sum')(x_hat, x)
+    recon_loss = nn.functional.cross_entropy(x_hat, x)
     # KL divergence
     epsilon = 1e-10
     kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - (logvar.exp() + epsilon))
 #     kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     return recon_loss, kld, recon_loss + kld
 
+x = torch.tensor(mci, dtype=torch.float32)
 
 # Training loop
 # for epoch in range(100):
 #     # Forward pass
-#     x = torch.tensor(mci.values, dtype=torch.float32)
+#
 #     x_hat, mu, logvar = model(x)
 #
 #     # Compute loss components
@@ -150,9 +262,10 @@ def loss_function(x_hat, x, mu, logvar):
 #         f'Total Loss: {total_loss.item():.4f}')
 
 # Training loop
-for epoch in range(100):
+for epoch in range(10000):
     # Forward pass
-    x = torch.tensor(mci.values, dtype=torch.float32)
+    # x = torch.tensor(train.values, dtype=torch.float32)
+    # x_val = torch.tensor(val.values, dtype=torch.float32)
     x_hat, mu, logvar = model(x)
 
     # Compute loss components
@@ -170,3 +283,6 @@ for epoch in range(100):
     # Print losses
     print(
         f'Epoch: {epoch + 1}, Recon Loss: {recon_loss.item():.4f}, KLD: {kld.item():.4f}, Total Loss: {total_loss.item():.4f}')
+
+
+
